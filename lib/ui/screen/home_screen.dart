a0c021fb806b3b8468dea62315deeb19f44af026
GregBiglione <gregory.biglione81@gmail.com>
2022-10-20 18:00:15 +0200

    Added Constant & AppBar in HomeScreen

--- lib/ui/screen/home_screen.dart
+++ lib/ui/screen/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:netflix/utils/constant.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -10,6 +11,13 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: kBackgroundColor,
+        leading: Image.asset(
+          "assets/images/netflix_logo_2.png"
+        ),
+      ),
+    );
   }
 }
