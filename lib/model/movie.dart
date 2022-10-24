35ffac9cd749b3fef922bd035e13c4a7783c28fd
GregBiglione <gregory.biglione81@gmail.com>
2022-10-24 13:41:33 +0200

    Added Movie object

---
+++ lib/model/movie.dart
@@ -0,0 +1,50 @@
+import 'package:netflix/service/api.dart';
+
+class Movie {
+  final int id;
+  final String name;
+  final String description;
+  final String? posterPath;
+
+//<editor-fold desc="Data Methods">
+
+  Movie({
+    required this.id,
+    required this.name,
+    required this.description,
+    this.posterPath,
+  });
+
+  Movie copyWith({
+    int? id,
+    String? name,
+    String? description,
+    String? posterPath,
+  }) {
+    return Movie(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      description: description ?? this.description,
+      posterPath: posterPath ?? this.posterPath,
+    );
+  }
+
+  factory Movie.fromJson(Map<String, dynamic> map) {
+    return Movie(
+      id: map['id'] as int,
+      name: map['title'] as String,
+      description: map['overview'] as String,
+      posterPath: map['poster_path'] as String,
+    );
+  }
+
+  // Poster url ****************************************************************
+
+  String posterUrl() {
+    Api api = Api();
+
+    return api.baseUrl + posterPath!;
+  }
+
+//</editor-fold>
+}
