dfef19308abc281abc30d0279dfb08356bf78111
GregBiglione <gregory.biglione81@gmail.com>
2022-10-24 12:57:22 +0200

    Added ApiService

---
+++ lib/service/api_service.dart
@@ -0,0 +1,28 @@
+import 'package:dio/dio.dart';
+import 'package:netflix/service/api.dart';
+
+class ApiService {
+  final Api api = Api();
+  final Dio dio = Dio();
+
+  Future<Response> getData(String path, {Map<String, dynamic>? params}) async {
+    String _url = api.baseUrl + path;
+    Map<String, dynamic> query = {
+      "api_key": api.apiKey,
+      "language": "fr-FR",
+    };
+
+    if(params != null) {
+      query.addAll(params);
+    }
+
+    final response = await dio.get(_url, queryParameters: query);
+
+    if(response.statusCode == 200) {
+      return response;
+    }
+    else {
+      throw response;
+    }
+  }
+}
