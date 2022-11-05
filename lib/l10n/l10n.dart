1f155e972e13890ffb9cef4afb75c0a32f9e34be
GregBiglione <gregory.biglione81@gmail.com>
2022-11-05 20:24:10 +0100

    Added danish translation & added app_da.arb

--- lib/l10n/l10n.dart
+++ lib/l10n/l10n.dart
@@ -3,6 +3,7 @@ import 'dart:ui';
 class L10n {
   static final all = [
     const Locale("bg"),
+    const Locale("da", "DK"),
     const Locale("de"),
     const Locale("en", "US"),
     const Locale("es"),
